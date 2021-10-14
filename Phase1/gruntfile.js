0c1cf82079bf45c8b0c812c763f7a525df1a7e78
MADAO-Byte <76618454+MADAO-Byte@users.noreply.github.com>
2021-10-14 12:34:45 +0530

    Update gruntfile.js

--- Phase1/gruntfile.js
+++ Phase1/gruntfile.js
@@ -1,39 +1,3 @@
-// module.exports = function (grunt) {
-//     grunt.initConfig({
-
-//     // define source files and their destinations
-//     uglify: {
-//         //files: { 
-//             all_src : { 
-//                 options : { 
-//                   sourceMap : true, 
-//                   sourceMapName : 'sourceMap.map'
-//                 }, 
-//             src: 'mysourcecode.site/js/*.js',  // source files mask
-//             dest: 'composite.all.min.js',    // destination folder
-            
-//             expand: true,    // allow dynamic building
-//             flatten: true,   // remove all unnecessary nesting
-//             //ext: '.min.js'   // replace .js to .min.js
-//             }
-//         },
-//     watch: {
-//         js:  { files: 'js/*.js', tasks: [ 'uglify' ] },
-//     }
-// });
-
-// // load plugins
-// grunt.loadNpmTasks('grunt-contrib-watch');
-// grunt.loadNpmTasks('grunt-contrib-uglify');
-
-// // register at least this one task
-// grunt.registerTask('default', [ 'uglify' ]);
-
-
-// };
-
-
-
 module.exports = function(grunt) {
 
     // This is the configuration.
